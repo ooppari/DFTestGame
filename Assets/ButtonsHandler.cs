ab0a1932ba13af890a72c53e665037e364f51cbf
Ooppari <ooppari@gmail.com>
2016-05-20 17:14:16 +0300

    FirstCommit To test project

---
+++ Assets/ButtonsHandler.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ButtonsHandler : MonoBehaviour {
+
+	// Create signleton to gamehandler later on
+	GameHandler m_gameHandler;
+	Color uglyColorRandom;
+
+	// Use this for initialization
+	void Start () {
+		m_gameHandler = Camera.main.GetComponent<GameHandler> ();
+		// Initial Color
+
+		Button b = GetComponent<Button> ();
+		ColorBlock cb = b.colors;
+
+		// Randomly create "ugly" colors. TODO: Change to picked colors array later on. 
+		uglyColorRandom = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f)) ;
+
+		// Put same color to all button colors to "look like" buttons not affected by user pressing
+		cb.normalColor = uglyColorRandom;
+		cb.highlightedColor = uglyColorRandom;
+		cb.pressedColor = uglyColorRandom;
+		cb.disabledColor = uglyColorRandom;
+		b.colors = cb;
+
+		}
+
+	public void ButtonPressed()
+		{
+		Debug.Log ("Button pressed: " + transform.name);
+		m_gameHandler.RegisterButtonPress (gameObject.transform.name );
+		}
+
+	// Blink now
+	public void Blink()
+		{
+		Button b = GetComponent<Button> ();
+		ColorBlock cb = b.colors;
+
+		// Put same color to all button colors to "look like" buttons not affected by user pressing
+		cb.normalColor = Color.white;
+		cb.highlightedColor =  Color.white;
+		cb.pressedColor =  Color.white;
+		cb.disabledColor =  Color.white;
+		b.colors = cb;
+
+		}
+
+	// Back to ugly color
+	public void Regular()
+		{
+		Button b = GetComponent<Button> ();
+		ColorBlock cb = b.colors;
+
+		// Put same color to all button colors to "look like" buttons not affected by user pressing
+		cb.normalColor = uglyColorRandom;
+		cb.highlightedColor =  uglyColorRandom;
+		cb.pressedColor =  uglyColorRandom;
+		cb.disabledColor =  uglyColorRandom;
+		b.colors = cb;
+
+		}
+	}	
---
+++ Assets/GameHandler.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour {
+
+	public enum GameState
+		{
+		EStart,
+		EBlinking, 
+		EUserTurn
+		};
+
+	// First and only button
+	public GameObject m_button; 
+
+	public Text m_uiText;
+	ArrayList buttons = new ArrayList();
+
+	ArrayList buttonsOrderGenerated = new ArrayList();
+
+	// GameState 
+	GameState m_gs;
+
+	bool StartBlink = false;
+	float timeToStartGame = 3f;
+	float blinkTime = 1f;
+	float coolDownTime = 0.5f;
+	int blinkButton;
+	int blinkTimes = 0;
+
+	int userPressToCheck;
+
+	// Use this for initialization
+	void Start () {
+		m_button.SetActive (false);
+		m_uiText.text = "Wait for blinks, when last blink is done, remember your decissions";
+		// Create initial buttons
+		for (int i = 0; i < 3; i++) 
+			{
+			GameObject but = Instantiate (m_button);
+			RectTransform bt = but.GetComponent<RectTransform> ();
+			but.name = "button" + i;
+			but.SetActive (true);
+
+			but.transform.position = new Vector3 (m_button.transform.position.x, 
+				m_button.transform.position.y + i*bt.rect.height, 
+				m_button.transform.position.z); 
+			but.transform.parent = m_button.transform.parent;
+
+			buttons.Add(but);
+			}
+		}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (m_gs == GameState.EStart) 
+			{
+			if (timeToStartGame > 0) 
+				{
+				timeToStartGame -= Time.deltaTime;
+				} 
+			else 
+				{
+				m_gs = GameState.EBlinking;
+				timeToStartGame = 3f;
+				blinkButton = Random.Range (0, buttons.Count);
+				GameObject but = (GameObject)buttons [blinkButton];
+				ButtonsHandler bt = but.GetComponent<ButtonsHandler> ();
+				bt.Blink ();
+				Debug.Log ("BO COUNT" + buttonsOrderGenerated.Count);
+				buttonsOrderGenerated.Add (bt.name);
+				m_uiText.text = "Follow blinks";
+
+				}
+			} 
+		else if (m_gs == GameState.EBlinking) 
+			{
+			if (blinkTime > 0) 
+				{
+				blinkTime -= Time.deltaTime;
+				} 
+			else if (coolDownTime > 0) 
+				{
+				coolDownTime -= Time.deltaTime;
+				GameObject but = (GameObject)buttons [blinkButton];
+				ButtonsHandler bt = but.GetComponent<ButtonsHandler> ();
+				bt.Regular ();
+				}
+			else 
+				{
+				blinkTime = 1f;
+				coolDownTime = 0.5f;
+				blinkTimes++;
+
+				if (blinkTimes < buttons.Count ) 
+					{
+					m_uiText.text = "Follow blinks" + blinkTimes;
+					blinkButton = Random.Range (0, buttons.Count);
+					GameObject butNew = (GameObject)buttons [blinkButton];
+					ButtonsHandler btNew = butNew.GetComponent<ButtonsHandler> ();
+					btNew.Blink ();
+
+					buttonsOrderGenerated.Add (btNew.name);
+					}
+				else
+					{
+					m_uiText.text = "Now you. Take your time" + blinkTimes;
+
+					m_gs = GameState.EUserTurn;
+					blinkTimes = 0;
+					}
+				}
+			}
+			
+	}
+
+	// Take button events and function based to those when gamestate is user turn. 
+	// Check if user is pressing in correct order.
+	public void RegisterButtonPress( string buttonName ) 
+		{
+		if ( m_gs == GameState.EUserTurn )
+			{
+			string nameOfButton = (string)buttonsOrderGenerated [userPressToCheck];
+			if ( nameOfButton == buttonName) 
+				{
+				Debug.Log ("Corret" );
+
+				userPressToCheck++;
+
+				m_uiText.text = "CORRECT" + userPressToCheck;
+
+				if (userPressToCheck == buttonsOrderGenerated.Count) 
+					{
+					m_uiText.text = "WICTORY!";
+					m_gs = GameState.EStart;
+					buttonsOrderGenerated.Clear();
+					userPressToCheck = 0;
+
+					// Add new button to row
+					GameObject but = Instantiate (m_button);
+					RectTransform bt = but.GetComponent<RectTransform> ();
+					but.name = "button" + buttons.Count;
+					but.SetActive (true);
+
+					but.transform.position = new Vector3 (m_button.transform.position.x, 
+						m_button.transform.position.y + buttons.Count*bt.rect.height, 
+						m_button.transform.position.z); 
+					but.transform.parent = m_button.transform.parent;
+
+					buttons.Add(but);
+					}
+				} 
+			else 
+				{
+				Debug.Log ("Fail" + nameOfButton + " Compare to " + buttonName );
+				m_uiText.text = "FAILED";
+				m_gs = GameState.EStart;
+				buttonsOrderGenerated.Clear();
+				buttonsOrderGenerated = new ArrayList ();
+				userPressToCheck = 0;
+				}
+
+			}
+		}
+	}
